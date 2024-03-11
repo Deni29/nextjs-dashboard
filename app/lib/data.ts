38cdc5cf7d8fa0fa079a5051ee68d559296441fe
deni29 <deni2929.dd@gmail.com>
2024-03-11 16:10:35 +0100

    Update invoice edit page to handle not found invoice.

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -64,9 +64,9 @@ export async function fetchCardData() {
     const invoiceCountPromise = sql`SELECT COUNT(*) FROM invoices`;
     const customerCountPromise = sql`SELECT COUNT(*) FROM customers`;
     const invoiceStatusPromise = sql`SELECT
-         SUM(CASE WHEN status = 'paid' THEN amount ELSE 0 END) AS "paid",
-         SUM(CASE WHEN status = 'pending' THEN amount ELSE 0 END) AS "pending"
-         FROM invoices`;
+        SUM(CASE WHEN status = 'paid' THEN amount ELSE 0 END) AS "paid",
+        SUM(CASE WHEN status = 'pending' THEN amount ELSE 0 END) AS "pending"
+        FROM invoices`;
 
     const data = await Promise.all([
       invoiceCountPromise,
@@ -172,7 +172,7 @@ export async function fetchInvoiceById(id: string) {
       status: invoice.status,
     }));
 
-    console.log(invoice[0]);
+    console.log(invoice); // Invoice is an empty array []
     return invoice[0];
   } catch (error) {
     console.error('Database Error:', error);
