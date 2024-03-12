0a3d0ce82562d68e22643aabace2b4820d37accb
deni29 <deni2929.dd@gmail.com>
2024-03-12 12:53:18 +0100

    Edit invoice page: Remove type guard function for InvoiceForm

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -2,7 +2,6 @@ import Form from '@/app/ui/invoices/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchInvoiceById, fetchCustomers } from '@/app/lib/data';
 import { notFound } from 'next/navigation';
-import { CustomerField, InvoiceForm } from '@/app/lib/definitions';
 
 export default async function Page({ params }: { params: { id: string } }) {
     const id = params.id;
@@ -15,35 +14,19 @@ export default async function Page({ params }: { params: { id: string } }) {
         notFound();
     }
 
-    if (invoice && isInvoiceForm(invoice)) {
-        return (
-            <main>
-                <Breadcrumbs
-                    breadcrumbs={[
-                        { label: 'Invoices', href: '/dashboard/invoices' },
-                        {
-                            label: 'Edit Invoice',
-                            href: `/dashboard/invoices/${id}/edit`,
-                            active: true,
-                        },
-                    ]}
-                />
-                <Form invoice={invoice} customers={customers} />
-            </main>
-        );
-    } else {
-        notFound();
-    }
-
-    // Define a type guard function to check if a value is of type InvoiceForm
-    function isInvoiceForm(value: any): value is InvoiceForm {
-        // Check if the value has all the required properties of InvoiceForm
-        return (
-            typeof value.id === 'string' &&
-            typeof value.customer_id === 'string' &&
-            typeof value.amount === 'number' &&
-            typeof value.status === 'string' &&
-            (value.status === 'pending' || value.status === 'paid')
-        );
-    }
+    return (
+        <main>
+            <Breadcrumbs
+                breadcrumbs={[
+                    { label: 'Invoices', href: '/dashboard/invoices' },
+                    {
+                        label: 'Edit Invoice',
+                        href: `/dashboard/invoices/${id}/edit`,
+                        active: true,
+                    },
+                ]}
+            />
+            <Form invoice={invoice} customers={customers} />
+        </main>
+    );
 }
